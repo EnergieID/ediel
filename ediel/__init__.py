fea84f48d0cff76479ec8cc12976a36b05e8aebd
Jan Pecinovsky <janpecinovsky@gmail.com>
2017-03-11 03:22:07 +0100

    setup parameters

--- ediel/__init__.py
+++ ediel/__init__.py
@@ -1,1 +1,3 @@
 from .uniformat import UNIBaseParser
+from .twowire import TwoWireParser, TwoWireMMRParser
+from .ediel import __version__
---
+++ ediel/ediel.py
@@ -0,0 +1,4 @@
+__title__ = "ediel"
+__version__ = "0.0.1"
+__author__ = "EnergieID.be"
+__license__ = "MIT"
