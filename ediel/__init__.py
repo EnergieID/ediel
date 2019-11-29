c28ed2678fbc1e0c44a943e7693b6894ebf4b737
Jan Pecinovsky <janpecinovsky@gmail.com>
2019-11-29 12:22:50 +0100

    improved imports

--- ediel/__init__.py
+++ ediel/__init__.py
@@ -1,3 +1,5 @@
 from .uniformat import UNIBaseParser
 from .twowire import TwoWireParser, TwoWireMMRParser
+from . import filename
+from . import mig
 from .ediel import __version__
--- ediel/ediel.py
+++ ediel/ediel.py
@@ -1,4 +1,4 @@
 __title__ = "ediel"
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 __author__ = "EnergieID.be"
 __license__ = "MIT"
