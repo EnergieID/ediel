5e603080f799395b2163adcf2e67a8e58b1fcbbb
Jan Pecinovsky <janpecinovsky@gmail.com>
2020-04-06 12:48:23 +0200

    TwoWireAMR parser

--- ediel/twowire.py
+++ ediel/twowire.py
@@ -1,6 +1,6 @@
 from cached_property import cached_property
 import pandas as pd
-from typing import Union
+from typing import Union, List, Dict, Tuple, Optional
 import io
 
 from .uniformat import UNIBaseParser
@@ -23,126 +23,31 @@ class TwoWireParser(UNIBaseParser):
             # so we want the second element, last part, and without the space
             return _format[1].split(': ')[1].replace(' ', '')
 
-    def get_timeseries_frame(self):
-        """
-        Returns
-        -------
-        pd.DataFrame
+    @property
+    def val_range(self) -> Tuple[int, Optional[int]]:
+        raise NotImplementedError
 
-        Raises
-        ------
-        NotImplementedError
-        """
-        raise NotImplementedError('Method needs to be implemented by subclass')
+    @property
+    def meta_range(self) -> Tuple[Optional[int], int]:
+        raise NotImplementedError
 
-    def get_metadata_frame(self):
-        """
-        Returns
-        -------
-        pd.DataFrame
-
-        Raises
-        ------
-        NotImplementedError
-        """
-        raise NotImplementedError('Method needs to be implemented by subclass')
-
-    def _parse_dataframe(self):
-        """
-        Returns
-        -------
-        pd.DataFrame
-
-        Raises
-        ------
-        NotImplementedError
-        """
-        raise NotImplementedError('Method needs to be implemented by subclass')
-
-
-class TwoWireMMRParser(TwoWireParser):
-    def __init__(self, file: Union[str, io.StringIO, io.FileIO]):
-        super(TwoWireMMRParser, self).__init__(file=file)
-        self.is_long_format = False
-
-    def _parse_dataframe(self):
-        """
-        Returns
-        -------
-        pd.DataFrame
-        """
-        def pandas_read(parse_dates, column_names):
-            with open_filename(filename=self.file, mode='r') as f:
-                _df = pd.read_csv(
-                    f,
-                    # because C parse does not support skipfooter:
-                    engine='python',
-                    header=None,
-                    skiprows=self.body_start_line,
-                    skipfooter=len(self.raw) - self.body_end_line - 1,
-                    sep=";",
-                    decimal=",",
-                    true_values=['yes'],
-                    false_values=['no'],
-                    parse_dates=parse_dates,
-                    date_parser=self._date_parser
-                )
-            _df.rename(columns=column_names, inplace=True)
-            _df.set_index('Name', inplace=True)
-            return _df
-
-        try:
-            df = pandas_read(
-                parse_dates=[5, 7],
-                column_names={
-                    0: 'Name',
-                    1: 'Type',
-                    2: 'Tariff',
-                    3: 'Cumulative',
-                    4: 'Unit',
-                    5: 'Start',
-                    7: 'End'
-                }
-            )
-        except ValueError:
-            df = pandas_read(
-                parse_dates=[6, 8],
-                column_names={
-                    0: 'Ean',
-                    1: 'Name',
-                    2: 'Type',
-                    3: 'Tariff',
-                    4: 'Cumulative',
-                    5: 'Unit',
-                    6: 'Start',
-                    8: 'End'
-                }
-            )
-            self.is_long_format = True
-
-        sorted_cols = sort_mixed_list(df.columns.tolist())
-        df = df[sorted_cols]
-        return df
-
-    def get_timeseries_frame(self, allow_duplicate_names=True):
+    def get_timeseries_frame(self, allow_duplicate_names: bool = True) -> pd.DataFrame:
         """
         Parameters
         ----------
         allow_duplicate_names : bool
             default True
             if False, only the first occurence of a name is returned
-            
+
         Returns
         -------
         pd.DataFrame
         """
         df = self.get_dataframe()
 
-        if not self.is_long_format:
-            vals = df[df.columns[6:]]
-        else:
-            vals = df[df.columns[7:-2]]
-            
+        val_from, val_to = self.val_range
+        vals = df[df.columns[val_from:val_to]]
+
         if not allow_duplicate_names:
             vals = vals.reset_index()
             vals = vals.drop_duplicates(subset='Name')
@@ -170,16 +75,14 @@ class TwoWireMMRParser(TwoWireParser):
         allow_duplicate_names : bool
             default True
             if False, only the first occurence of a name is returned
-            
+
         Returns
         -------
         pd.DataFrame
         """
         df = self.get_dataframe()
-        if not self.is_long_format:
-            meta = df[df.columns[:6]]
-        else:
-            meta = df[df.columns[:7]]
+        meta_from, meta_to = self.meta_range
+        meta = df[df.columns[meta_from:meta_to]]
 
         if not allow_duplicate_names:
             meta = meta.reset_index()
@@ -187,3 +90,125 @@ class TwoWireMMRParser(TwoWireParser):
             meta = meta.set_index('Name')
 
         return meta.T
+
+    def _parse_dataframe(self):
+        """
+        Returns
+        -------
+        pd.DataFrame
+
+        Raises
+        ------
+        NotImplementedError
+        """
+        raise NotImplementedError('Method needs to be implemented by subclass')
+
+    def _pandas_read(self, parse_dates: List[int], column_names: Dict):
+        with open_filename(filename=self.file, mode='r') as f:
+            _df = pd.read_csv(
+                f,
+                # because C parse does not support skipfooter:
+                engine='python',
+                header=None,
+                skiprows=self.body_start_line,
+                skipfooter=len(self.raw) - self.body_end_line - 1,
+                sep=";",
+                decimal=",",
+                true_values=['yes'],
+                false_values=['no'],
+                parse_dates=parse_dates,
+                date_parser=self._date_parser
+            )
+        _df.rename(columns=column_names, inplace=True)
+        _df.set_index('Name', inplace=True)
+
+        sorted_cols = sort_mixed_list(_df.columns.tolist())
+        _df = _df[sorted_cols]
+
+        return _df
+
+
+class TwoWireMMRParser(TwoWireParser):
+    def __init__(self, file: Union[str, io.StringIO, io.FileIO]):
+        super(TwoWireMMRParser, self).__init__(file=file)
+        self.is_long_format = False
+
+    def _parse_dataframe(self):
+        """
+        Returns
+        -------
+        pd.DataFrame
+        """
+        try:
+            df = self._pandas_read(
+                parse_dates=[5, 7],
+                column_names={
+                    0: 'Name',
+                    1: 'Type',
+                    2: 'Tariff',
+                    3: 'Cumulative',
+                    4: 'Unit',
+                    5: 'Start',
+                    7: 'End'
+                }
+            )
+        except ValueError:
+            df = self._pandas_read(
+                parse_dates=[6, 8],
+                column_names={
+                    0: 'Ean',
+                    1: 'Name',
+                    2: 'Type',
+                    3: 'Tariff',
+                    4: 'Cumulative',
+                    5: 'Unit',
+                    6: 'Start',
+                    8: 'End'
+                }
+            )
+            self.is_long_format = True
+
+        return df
+
+    @property
+    def val_range(self) -> Tuple[int, Optional[int]]:
+        if not self.is_long_format:
+            return 6, None
+        else:
+            return 7, -2
+
+    @property
+    def meta_range(self) -> Tuple[Optional[int], int]:
+        if not self.is_long_format:
+            return None, 6
+        else:
+            return None, 7
+
+
+class TwoWireAMRParser(TwoWireParser):
+    @property
+    def val_range(self) -> Tuple[int, Optional[int]]:
+        return 5, 102
+
+    @property
+    def meta_range(self) -> Tuple[Optional[int], int]:
+        return None, 5
+
+    def _parse_dataframe(self) -> pd.DataFrame:
+        df = self._pandas_read(
+            parse_dates=[0, 1],
+            column_names={
+                0: 'Start',
+                1: 'End',
+                2: 'Ean',
+                3: 'Name',
+                4: 'Type',
+                5: 'Unit'
+            }
+        )
+        return df
+
+    def get_timeseries_frame(self, allow_duplicate_names: bool = True) -> pd.DataFrame:
+        df = super(TwoWireAMRParser, self).get_timeseries_frame(allow_duplicate_names=allow_duplicate_names)
+        df.drop(df.last_valid_index(), inplace=True)
+        return df
